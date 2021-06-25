d430e2579ebabd4b613e26bd6e37860df8cf29b3
PujanGupta <onlinelearning@outlook.sg>
2021-06-25 10:32:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+def checkIfPrime(n):
+    prime = True
+    for i in range(2, n - 1):
+        if n % i == 0:
+            prime = False
+            break
+        else:
+            pass
+        
+    if prime == True:
+        return "Prime"
+    elif prime == False:
+        return "Composite"
+
+while True:
+    userInput = int(input("Enter a Number: "))
+    userInputStatus = checkIfPrime(userInput)
+
+    print(str(userInput) + " a " + userInputStatus + " Number !!")
+
